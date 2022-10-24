9734c95ad2941edc3a3548e13d65acf768af1aae
Cristina Claver <74592926+cclaver001@users.noreply.github.com>
2022-10-24 07:18:24 +0200

    Update 1_Biodiversity_checklist_assembly.R

--- 1_Biodiversity_checklist_assembly.R
+++ 1_Biodiversity_checklist_assembly.R
@@ -1,93 +1,91 @@
-###########################################################################
-
 # REQUIRED LIBRARIES
 library(readxl)
 library(taxize)
 library(Orcs)
 library(tidyverse)
 library(usefun)
+library(RSQLite)
+library(rfishbase) #if problems with rfishbase, try unlink(rfishbase:::db_dir())
+library(DBI)
 
 #########################################################################
 ###########################################################################
 # CREATION OF THE FISH BIODIVERSITY CHECKLIST 
-setwd()
-setwd("~/Escritorio/Cristina/Datos/2021/")
 
-#Load data
-Baltic_Sea <- read_excel("FishBase_LME.xlsx", sheet = "Baltic Sea")
-Barents_Sea <-read_excel("FishBase_LME.xlsx", sheet = "Barents Sea")
-Black_Sea <-read_excel("FishBase_LME.xlsx", sheet = "Black Sea")
-Canary_Current <-read_excel("FishBase_LME.xlsx", sheet = "Canary current")
-Celtic_Biscay_Shelf <-read_excel("FishBase_LME.xlsx", sheet = "Celtic Biscay Shelf")
-Faroe_Plateau <-read_excel("FishBase_LME.xlsx", sheet = "Faroe Plateau")
-Greenland_Sea <-read_excel("FishBase_LME.xlsx", sheet = "Greenland Sea")
-Iberian_Coastal <-read_excel("FishBase_LME.xlsx", sheet = "Iberian Coastal")
-Iceland_Shelf_and_Sea <-read_excel("FishBase_LME.xlsx", sheet = "Iceland Shelf and Sea")
-North_Sea <-read_excel("FishBase_LME.xlsx", sheet = "North Sea")
-Norwegian_Sea <-read_excel("FishBase_LME.xlsx", sheet = "Norwegian Sea")
-Mediterranean_Sea <-read_excel("FishBase_LME.xlsx", sheet = "Mediterranean Sea")
+setwd("~/Escritorio/Cristina/Datos/2021/Pipeline/Step 1/")
+
+# Import data from fishbase
+Baltic_Sea <-species_by_ecosystem(ecosystem = "Baltic Sea", server = "fishbase")
+Barents_Sea <- species_by_ecosystem(ecosystem = "Barents Sea", server = "fishbase")
+Black_Sea <-species_by_ecosystem(ecosystem = "Black Sea", server = "fishbase")
+Canary_Current <-species_by_ecosystem(ecosystem = "Canary Current", server = "fishbase")
+Celtic_Biscay_Shelf <-species_by_ecosystem(ecosystem = "Celtic-Biscay Shelf", server = "fishbase")
+Faroe_Plateau <-species_by_ecosystem(ecosystem = "Faroe Plateau", server = "fishbase")
+Greenland_Sea <-species_by_ecosystem(ecosystem = "Greenland Sea", server = "fishbase")
+Iberian_Coastal <-species_by_ecosystem(ecosystem = "Iberian Coastal", server = "fishbase")
+Iceland_Shelf_and_Sea <-species_by_ecosystem(ecosystem = "Iceland Shelf and Sea", server = "fishbase")
+North_Sea <-species_by_ecosystem(ecosystem = "North Sea", server = "fishbase")
+Norwegian_Sea <-species_by_ecosystem(ecosystem = "Norwegian Sea", server = "fishbase")
+Mediterranean_Sea<-species_by_ecosystem(ecosystem = "Mediterranean Sea", server = "fishbase")
 
 # Merge data
 df <- list(data.frame(Baltic_Sea), data.frame(Barents_Sea), data.frame(Black_Sea), data.frame(Canary_Current),
             data.frame(Celtic_Biscay_Shelf),data.frame(Faroe_Plateau), data.frame(Greenland_Sea), data.frame(Iberian_Coastal),
             data.frame(Iceland_Shelf_and_Sea), data.frame(North_Sea), data.frame(Norwegian_Sea), data.frame(Mediterranean_Sea))
 species_fishbase <- merge(df, by = "Species", all = TRUE)
-species_fishbase <- species_fishbase$Species
+species_fishbase <- data.frame(unique(species_fishbase$Species))
+names(species_fishbase)<-"Species"
+
+# Remove subspecies
+species_fishbase <- species_fishbase %>% 
+  separate(col=Species, into = c("genus","species","subspecies"), sep = " ") %>%
+  unite(col = "Species", genus, species, sep = " ") %>% dplyr::select(Species)
+species_fishbase <- unique(species_fishbase)
 
 # Retrieve taxonomy from WORMS 
-taxonomy <- classification(species_fishbase, db="worms")
-class(taxonomy)
-#Warning: Not found 12 species*
+taxonomy <- classification(species_fishbase$Species, db="worms", marine_only=FALSE)
 
 #Turn classification object into dataframe
 nombre <- c()
-output <- array(dim = c(length(species_fishbase),7))
-colnames(output) <- c("Kingdom", "Phylum", "Class", "Order", "Family", "Genus", "Species")
+output <- array(dim = c(dim(species_fishbase)[1],7))
+colnames(output) <- c("Kingdom", "Phylum", "Class_Gigaclass", "Order", "Family", "Genus", "Species")
 for (i in seq(1,length(taxonomy))) {
   nombre <- as.character(names(taxonomy[i]))
-  if (!is.na(taxonomy[nombre])) {
-    output[i,] <- t(eval(parse(text = paste0("taxonomy$`",nombre, "`$name[taxonomy$`", nombre, "`$rank == 'Kingdom' |",
-                                             "taxonomy$`", nombre, "`$rank == 'Phylum' |",
-                                             "taxonomy$`", nombre, "`$rank == 'Class' |", 
-                                             "taxonomy$`", nombre, "`$rank == 'Order' |",
-                                             "taxonomy$`", nombre, "`$rank == 'Family' |",
-                                             "taxonomy$`", nombre, "`$rank == 'Genus' |", 
-                                             "taxonomy$`", nombre, "`$rank == 'Species']" ))))
+  if (!is.na(taxonomy[nombre])) { 
+    if (sum(as.integer(eval(parse(text = paste0("taxonomy$`",nombre,"`$rank == 'Class'"))))) == 1) {
+      output[i,] <- t(eval(parse(text = paste0("taxonomy$`",nombre, "`$name[taxonomy$`", nombre, "`$rank == 'Kingdom' |",
+                                               "taxonomy$`", nombre, "`$rank == 'Phylum' |",
+                                               "taxonomy$`", nombre, "`$rank == 'Class' |", 
+                                               "taxonomy$`", nombre, "`$rank == 'Order' |",
+                                               "taxonomy$`", nombre, "`$rank == 'Family' |",
+                                               "taxonomy$`", nombre, "`$rank == 'Genus' |", 
+                                               "taxonomy$`", nombre, "`$rank == 'Species']" ))))
+    } else {
+      output[i,] <- t(eval(parse(text = paste0("taxonomy$`",nombre, "`$name[taxonomy$`", nombre, "`$rank == 'Kingdom' |",
+                                               "taxonomy$`", nombre, "`$rank == 'Phylum' |",
+                                               "taxonomy$`", nombre, "`$rank == 'Gigaclass' |",
+                                               "taxonomy$`", nombre, "`$rank == 'Order' |",
+                                               "taxonomy$`", nombre, "`$rank == 'Family' |",
+                                               "taxonomy$`", nombre, "`$rank == 'Genus' |",
+                                               "taxonomy$`", nombre, "`$rank == 'Species']" ))))
+    }
+    
   }
   rm(nombre)
 }
 output <- data.frame(output)
-head(output)
+order_clean <- data.frame(gsub("\\ .*","",output$Order))
+output<-cbind(output, order_clean)
+output<-output[, c(1,2,3,8,4,5,6,7)]
+output$Order<-NULL
+names(output)[4]<- "Order"
+output<-output[complete.cases(output),]# LIST OF MARINE EUROPEAN FISH SPECIES
 
-# Check those "Not found records"
-nrow(distinct(as.data.frame(species_fishbase))) - nrow(distinct(output)) #input - output
+# Export data
+write.table(output, "Species_list.txt", sep = " ",
+            row.names = FALSE, col.names = TRUE)
 
-found.sp <- distinct(output) 
-found.sp <- found.sp$Species # species that were found in WORMS and whose taxonomy is compiled
-notfound.sp <- as.data.frame(outersect(species_fishbase, found.sp))
-names(notfound.sp) <- "Species"
-head(notfound.sp) #there are some subspecies
-notfound.sp <- separate(notfound.sp, Species, into = c("genus","species","subspecies"), sep =" ")
-notfound.sp <- as.data.frame(paste(notfound.sp$genus,"",notfound.sp$species))
-names(notfound.sp)<- "Species"
-notfound.sp <- distinct(notfound.sp) #subspecies removed, now check again in worms
-notfound.sp <- notfound.sp$Species
-notfound.sp <- as.character(notfound.sp)
-notfound.sp <- str_squish(notfound.sp)
-taxonomy2 <- classification(notfound.sp, db="worms")
-# Again, we get 12 not found species, which are not marine. e.g. Barbatula barbatula is freschwater.
-
-# Check those found 8 species 
-found2 <- map(taxonomy2,~data.frame(.))
-found2 <- map_dfr(found2,~mutate_all(.,as.character)) 
-found2 <- found2[found2$rank =="Species",]
-found2 <- found2$name
-found2<-found2[!is.na(found2)]
-length(intersect(output$Species, found2))
-# This species were found in the first classification() but in our species_fishbase file we had both subspecies
-#and species e.g. Stomias boa, Stomias boa boa, Stomias boa ferox being multiplpe records for the same species.
-
-output <- unique(output) # LIST OF MARINE EUROPEAN FISH SPECIES
-write.csv(output, file = "Species_list.csv")
-write.table(output, "Species_list.txt", sep = " ", dec = ".",
-            row.names = TRUE, col.names = TRUE)
+# Create species list to retrieve sequence information from NCBI 
+sp_names <- data.frame(output$Species)
+write.table(sp_names, "sp_names.txt",
+            row.names = FALSE, col.names = FALSE)
